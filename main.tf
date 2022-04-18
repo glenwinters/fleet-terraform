f18b49274cf95cb067355810bbbf0c801b7f9cdc
Glen Winters <glenwinters@gmail.com>
2022-04-18 01:15:13 -0500

    feat: add artifact registry for fleet-graph-gateway

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-data "google_project" "fleet" {}
-
-output "project_number" {
-  value = data.google_project.fleet.number
+# TODO use a loop to generate registries based on a list of names
+resource "google_artifact_registry_repository" "fleet_graph_gateway_registry" {
+  repository_id = "fleet-graph-gateway"
+  format        = "DOCKER"
 }
--- provider.tf
+++ provider.tf
@@ -3,3 +3,9 @@ provider "google" {
   project      = var.gcp_project_id
   region       = "us-central1"
 }
+
+provider "google-beta" {
+  access_token = var.gcp_access_token
+  project      = var.gcp_project_id
+  region       = "us-central1"
+}
