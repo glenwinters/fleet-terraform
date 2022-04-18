e561fd989870340c606a320a4ec96fc739785fe3
Glen Winters <glenwinters@gmail.com>
2022-04-18 01:22:23 -0500

    fix: add missing location to artifact registry

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ resource "google_artifact_registry_repository" "fleet_graph_gateway_registry" {
   provider = google-beta
 
   repository_id = "fleet-graph-gateway"
+  location      = "us-central1"
   format        = "DOCKER"
 }
