fa461188a068cc09874ac0915d65532d9cc196f2
Glen Winters <glenwinters@gmail.com>
2022-04-18 01:17:01 -0500

    fix: use google-beta for the artifact repository

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 # TODO use a loop to generate registries based on a list of names
 resource "google_artifact_registry_repository" "fleet_graph_gateway_registry" {
+  provider = google-beta
+
   repository_id = "fleet-graph-gateway"
   format        = "DOCKER"
 }
